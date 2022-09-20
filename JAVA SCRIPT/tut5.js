c64dad34d6b28d41a6b85a3ac44d250f1c17719b
sunny <sengersunny448@gmail.com>
2022-09-20 16:23:14 +0530

    web development

---
+++ JAVA SCRIPT/tut5.js
@@ -0,0 +1,3 @@
+console.log("hey coder's how's you ");
+
+let status=prompt("Never get attached :) ");
---
+++ JAVA SCRIPT/tut7.js
@@ -0,0 +1,26 @@
+// var arr=[1,2,3,4,5,6];
+// console.log(1 - '1')
+// // console.log(arr);
+// var a = 2 ; 
+// var b = 3 ;
+// console.log(++a + ++b);
+// arr["four"]=90;
+
+
+//  for(num in arr){
+//     // console.log(num,":",arr[num]);
+//  }
+{
+   var a="sunny";
+
+   let c="hola amigos";
+
+}
+
+console.log(a);
+
+console.log(c);
+
+
+
+
