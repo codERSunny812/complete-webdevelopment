bda936cdc10b99c289c0ec037360b834079d7c4b
codersunny812 <sengersunny448@gmial.com>
2022-11-13 02:20:11 +0530

    JS

---
+++ comple js /tutorial12.js
@@ -0,0 +1,22 @@
+// arrays in javascript
+
+// very useful in real world application 
+
+const arr = [
+{ name:"sunny", age:19,gender:"male",hobbies:"vlogging and coding "},
+{name:"shiv", age:9,gender:"male",hobbies:"dance and coding "},
+{name:"sethu", age:19,gender:"male",hobbies:"vlogging and coding "},
+{name:"varun", age:19,gender:"male",hobbies:"vlogging and coding "}
+
+
+]
+
+console.log(arr);
+
+
+// how to itrrate this 
+
+for(let val of arr){
+    console.log(val.name);
+}
+
