92ba9f8c7e6d10cc40a152f56157a5349c1578fd
codersunny812 <sengersunny448@gmial.com>
2022-11-16 03:30:02 +0530

    tutorial 4

--- comple js /tutorial4.js
+++ comple js /tutorial4.js
@@ -1,31 +1,60 @@
 // String indexing 
+
+
 let firstName="Sunny";
-console.log(firstName[0]); //string slicing 
+
+console.log(firstName[4]); 
+
+//string slicing 
+
 console.log(firstName.length);
 
 // now lets know about strings method 
 
 // 1. trim   - to remove extra spaces from a string 
 
-let firstNames = "   sunny     ";
+let firstNames = "   sunny  pandey   ";
 console.log(firstNames.trim());
 
 
 // string slicing 
-let varNum = "hey amigos im sunny here ";
-console.log(varNum.slice(0,2));
 
+let varNum = "hey amigos im sunny here ";
+
+// There are 3 method for extracting a part of string 
+
+console.log(varNum.slice(0,9));
+
+console.log(varNum.substring(0,9));
+
+console.log(varNum.substr(0,9));
 
 // change num to string 
 var num = 12;
 var name = "sunny";
-console.log(num + " ");
+
+console.log(typeof (num+""));
 
 //change string into a number 
 let str = "sunny "; //here str is string here 
 
 sun = +"sunny "; //to  convert the string into a number  
 ;
+
+
+// string concat
+
+var fnames="sunny ";
+var pname ="vlogs";
+console.log(fnames.concat( " "+pname));
+
+
+
+
+
+
+
+
 
 //template string 
 
